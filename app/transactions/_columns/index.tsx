5415a98f4deb874d923965627b86f4af334f2ec7
Rafael Oto <rafaelotoso@gmail.com>
2024-11-06 16:48:44 -0300

    feat: ensure transaction table labels are rendered properly

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -1,8 +1,45 @@
 "use client";
 
-import { Transaction } from "@prisma/client";
+import {
+  PaymentMethod,
+  Transaction,
+  TransactionCategory,
+} from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import TransactionTypeBadge from "../_components/type-badge";
+import { Button } from "@/app/_components/ui/button";
+import { PencilIcon, TrashIcon } from "lucide-react";
+
+const transactionCategoryMap = {
+  [TransactionCategory.CASHBACK]: "Cashback",
+  [TransactionCategory.CLOTHING]: "Roupa",
+  [TransactionCategory.EDUCATION]: "Educação",
+  [TransactionCategory.ENTERTAINMENT]: "Lazer",
+  [TransactionCategory.FOOD]: "Comida",
+  [TransactionCategory.GIFT]: "Presente",
+  [TransactionCategory.GOALS]: "Metas",
+  [TransactionCategory.HEALTH]: "Saúde",
+  [TransactionCategory.INVESTMENT]: "Aporte de investimento",
+  [TransactionCategory.LIVING]: "Moradia",
+  [TransactionCategory.LOAN]: "Empréstimo",
+  [TransactionCategory.OTHER]: "Outros",
+  [TransactionCategory.PERSONAL]: "Pessoal",
+  [TransactionCategory.PET]: "Pet",
+  [TransactionCategory.REWARD]: "Prêmio",
+  [TransactionCategory.RETURN_ON_INVESTMENT]: "Rendimento de investimento",
+  [TransactionCategory.SALE]: "Venda",
+  [TransactionCategory.SALARY]: "Salário",
+  [TransactionCategory.SUBSCRIPTION]: "Assinatura",
+  [TransactionCategory.TAXES]: "Imposto",
+  [TransactionCategory.TRANSPORTATION]: "Transporte",
+  [TransactionCategory.TRAVEL]: "Viagem",
+  [TransactionCategory.VARIABLE]: "Variável",
+};
+
+const paymentMethodMap = {
+  [PaymentMethod.CREDIT]: "Crédito",
+  [PaymentMethod.DEBIT]: "Débito",
+};
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -19,21 +56,50 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "category",
     header: "Categoria",
+    cell: ({ row: { original: transaction } }) => (
+      <p>{transactionCategoryMap[transaction.category]}</p>
+    ),
   },
   {
     accessorKey: "paymentMethod",
     header: "Método de pagamento",
+    cell: ({ row: { original: transaction } }) => (
+      <p>{paymentMethodMap[transaction.paymentMethod]}</p>
+    ),
   },
   {
     accessorKey: "date",
     header: "Data",
+    cell: ({ row: { original: transaction } }) =>
+      new Date(transaction.date).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
   },
   {
     accessorKey: "amount",
     header: "Valor",
+    cell: ({ row: { original: transaction } }) =>
+      new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(Number(transaction.amount)),
   },
   {
     accessorKey: "actions",
     header: "",
+    cell: () => {
+      return (
+        <div className="space-x-1">
+          <Button variant="ghost" size="icon" className="text-muted-foreground">
+            <PencilIcon />
+          </Button>
+          <Button variant="ghost" size="icon" className="text-muted-foreground">
+            <TrashIcon />
+          </Button>
+        </div>
+      );
+    },
   },
 ];
--- app/transactions/_components/type-badge.tsx
+++ app/transactions/_components/type-badge.tsx
@@ -18,7 +18,7 @@ const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
 
   if (transaction.type === TransactionType.EXPENSE) {
     return (
-      <Badge className="bg-danger bg-opacity-10 font-bold text-danger">
+      <Badge className="bg-danger bg-opacity-10 font-bold text-danger hover:bg-danger hover:bg-opacity-10">
         <CircleIcon className="mr-2 fill-danger" size={10} />
         Despesa
       </Badge>
@@ -27,7 +27,7 @@ const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
 
   if (transaction.type === TransactionType.TRANSFER) {
     return (
-      <Badge className="bg-white bg-opacity-10 font-bold text-white">
+      <Badge className="bg-white bg-opacity-10 font-bold text-white hover:bg-white hover:bg-opacity-10">
         <CircleIcon className="mr-2 fill-white" size={10} />
         Transferência
       </Badge>
@@ -36,7 +36,7 @@ const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
 
   if (transaction.type === TransactionType.INVESTMENT) {
     return (
-      <Badge className="bg-yellow-500 bg-opacity-10 font-bold text-yellow-500">
+      <Badge className="bg-yellow-500 bg-opacity-10 font-bold text-yellow-500 hover:bg-yellow-500 hover:bg-opacity-10">
         <CircleIcon className="mr-2 fill-yellow-500" size={10} />
         Aporte
       </Badge>
@@ -45,7 +45,7 @@ const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
 
   if (transaction.type === TransactionType.RESCUE) {
     return (
-      <Badge className="bg-yellow-300 bg-opacity-10 font-bold text-yellow-300">
+      <Badge className="bg-yellow-300 bg-opacity-10 font-bold text-yellow-300 hover:bg-yellow-300 hover:bg-opacity-10">
         <CircleIcon className="mr-2 fill-yellow-300" size={10} />
         Resgate
       </Badge>
