b7ff9cd1128c1596f44d664031721ac409ddd9e4
Rafael Oto <rafaelotoso@gmail.com>
2024-11-06 16:11:30 -0300

    feat: add transaction type badge

---
+++ app/transactions/_components/type-badge.tsx
@@ -0,0 +1,56 @@
+import { Badge } from "@/app/_components/ui/badge";
+import { Transaction, TransactionType } from "@prisma/client";
+import { CircleIcon } from "lucide-react";
+
+interface TransactionTypeBadgeProps {
+  transaction: Transaction;
+}
+
+const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
+  if (transaction.type === TransactionType.GAIN) {
+    return (
+      <Badge className="bg-muted font-bold text-primary hover:bg-muted">
+        <CircleIcon className="mr-2 fill-primary" size={10} />
+        Ganho
+      </Badge>
+    );
+  }
+
+  if (transaction.type === TransactionType.EXPENSE) {
+    return (
+      <Badge className="bg-danger bg-opacity-10 font-bold text-danger">
+        <CircleIcon className="mr-2 fill-danger" size={10} />
+        Despesa
+      </Badge>
+    );
+  }
+
+  if (transaction.type === TransactionType.TRANSFER) {
+    return (
+      <Badge className="bg-white bg-opacity-10 font-bold text-white">
+        <CircleIcon className="mr-2 fill-white" size={10} />
+        Transferência
+      </Badge>
+    );
+  }
+
+  if (transaction.type === TransactionType.INVESTMENT) {
+    return (
+      <Badge className="bg-yellow-500 bg-opacity-10 font-bold text-yellow-500">
+        <CircleIcon className="mr-2 fill-yellow-500" size={10} />
+        Aporte
+      </Badge>
+    );
+  }
+
+  if (transaction.type === TransactionType.RESCUE) {
+    return (
+      <Badge className="bg-yellow-300 bg-opacity-10 font-bold text-yellow-300">
+        <CircleIcon className="mr-2 fill-yellow-300" size={10} />
+        Resgate
+      </Badge>
+    );
+  }
+};
+
+export default TransactionTypeBadge;
