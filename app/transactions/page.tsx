109c458d39ac19d5c32f48a5af3ff32855fe0e4f
Rafael Oto <rafaelotoso@gmail.com>
2024-11-07 22:20:50 -0300

    fix: console transaction type error

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -14,7 +14,10 @@ const TransactionsPage = async () => {
         <AddTransactionButton />
       </div>
 
-      <DataTable columns={transactionColumns} data={transactions} />
+      <DataTable
+        columns={transactionColumns}
+        data={JSON.parse(JSON.stringify(transactions))}
+      />
     </div>
   );
 };
