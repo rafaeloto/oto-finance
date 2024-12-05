189dd11b0bdac17ebbb2313ddda6967899b8c08d
Rafael Oto <rafaelotoso@gmail.com>
2024-12-04 22:54:27 -0300

    feat: add confirmation dialog to balance recalculation flow

--- app/accounts/_components/recalculate-balances-button.tsx
+++ app/accounts/_components/recalculate-balances-button.tsx
@@ -1,6 +1,17 @@
 "use client";
 
 import { recalculateBalances } from "@/app/_actions/accounts/recalculate-balances";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/app/_components/ui/alert-dialog";
 import { Button } from "@/app/_components/ui/button";
 import { CalculatorIcon, LoaderCircleIcon } from "lucide-react";
 import { useState } from "react";
@@ -9,7 +20,7 @@ import { toast } from "sonner";
 const RecalculateBalancesButton = () => {
   const [loading, setLoading] = useState(false);
 
-  const handleRecalculate = async () => {
+  const handleConfirmRecalculate = async () => {
     setLoading(true);
 
     try {
@@ -24,26 +35,46 @@ const RecalculateBalancesButton = () => {
   };
 
   return (
-    <>
-      <Button
-        className="rounded-full font-bold"
-        variant="destructive"
-        disabled={loading}
-        onClick={handleRecalculate}
-      >
-        {loading ? (
-          <>
-            <span>Recalculando...</span>
-            <LoaderCircleIcon className="animate-spin" />
-          </>
-        ) : (
-          <>
-            <span>Recalcular saldos</span>
-            <CalculatorIcon />
-          </>
-        )}
-      </Button>
-    </>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button
+          className="rounded-full font-bold"
+          variant="destructive"
+          disabled={loading}
+        >
+          {loading ? (
+            <>
+              <span>Recalculando...</span>
+              <LoaderCircleIcon className="animate-spin" />
+            </>
+          ) : (
+            <>
+              <span>Recalcular saldos</span>
+              <CalculatorIcon />
+            </>
+          )}
+        </Button>
+      </AlertDialogTrigger>
+
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Você deseja realmente recalcular os saldos?
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            Essa ação não pode ser desfeita. Irá recalcular e sobrescrever
+            permanentemente os saldos das contas nos nossos servidores.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogAction onClick={handleConfirmRecalculate}>
+            Continuar
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
 
