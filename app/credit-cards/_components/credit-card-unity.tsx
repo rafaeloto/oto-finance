89fbd90e858ec8de3fada3060592ae9b4c72a304
Rafael Oto <rafaelotoso@gmail.com>
2024-12-05 14:10:29 -0300

    feat: improve credit card component styles and data

--- app/credit-cards/_components/credit-card-unity.tsx
+++ app/credit-cards/_components/credit-card-unity.tsx
@@ -1,34 +1,92 @@
 import { CreditCard } from "@prisma/client";
-import { Card, CardContent } from "@/app/_components/ui/card";
+import { Card } from "@/app/_components/ui/card";
 import Image from "next/image";
+import { clerkClient } from "@clerk/nextjs/server";
+import { format } from "date-fns";
 
 interface CreditCardUnityProps {
   creditCard: CreditCard;
 }
 
-const CreditCardUnity = ({ creditCard }: CreditCardUnityProps) => {
-  return (
-    <Card className="flex max-w-[400px] items-center justify-around bg-white bg-opacity-5 p-10">
-      {/* Imagem à esquerda */}
-      <Image
-        src={`/credit-cards/${creditCard.flag}.svg`}
-        alt={creditCard.flag}
-        width={64}
-        height={64}
-      />
+const CreditCardUnity = async ({ creditCard }: CreditCardUnityProps) => {
+  const user = await clerkClient().users.getUser(creditCard.userId);
 
-      {/* Conteúdo à direita */}
-      <CardContent className="flex flex-col justify-between gap-3 p-6">
-        <p className="text-lg font-semibold text-white opacity-70">
-          {creditCard.name}
+  // Helper function to calculate closing and due dates
+  const calculateDate = (day: number): string => {
+    const today = new Date();
+    const currentMonth = today.getMonth();
+    const year = today.getFullYear();
+    const month =
+      day >= today.getDate()
+        ? currentMonth // Dia está no futuro: mês atual
+        : currentMonth + 1; // Dia já passou: próximo mês
+
+    const date = new Date(year, month, day);
+    return format(date, "dd/MM");
+  };
+
+  // List of possible gradients
+  const gradients = [
+    "from-black via-gray-950 to-black",
+    "from-gray-700 via-gray-800 to-gray-900",
+    "from-red-700 via-red-800 to-red-900",
+    "from-blue-700 via-blue-800 to-blue-900",
+    "from-green-700 via-green-800 to-green-900",
+    "from-purple-700 via-purple-800 to-purple-900",
+    "from-yellow-700 via-yellow-800 to-yellow-900",
+  ];
+
+  // Random gradient selection
+  const randomGradient =
+    gradients[Math.floor(Math.random() * gradients.length)];
+
+  return (
+    <Card
+      className={`flex h-[220px] w-[400px] flex-col justify-between rounded-2xl bg-gradient-to-r p-5 ${randomGradient}`}
+    >
+      {/* Cart name and flag logo*/}
+      <div className="flex items-center justify-between">
+        <p className="text-lg font-semibold">{creditCard.name}</p>
+
+        <Image
+          src={`/credit-cards/${creditCard.flag}.svg`}
+          alt={creditCard.flag}
+          width={48}
+          height={48}
+        />
+      </div>
+
+      {/* Fake number and chip */}
+      <div className="flex items-center justify-between">
+        <p className="font-mono text-lg tracking-widest">XXXX XXXX XXXX XXXX</p>
+
+        <div className="flex h-8 w-10 items-center justify-center rounded-sm bg-gradient-to-b from-yellow-500 to-yellow-700 shadow-inner">
+          <Image src="chip.svg" alt="chip" width={40} height={32} />
+        </div>
+      </div>
+
+      {/* User name, closing and due date of the invoice */}
+      <div className="flex items-center justify-between">
+        <p className="mt-2 text-base font-semibold uppercase">
+          {user.fullName || "USUÁRIO DESCONHECIDO"}
         </p>
-        <p className="text-center text-2xl font-bold">
-          {Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-          }).format(Number(creditCard.limit))}
-        </p>
-      </CardContent>
+
+        <div className="flex gap-5">
+          <div className="flex flex-col items-center justify-center">
+            <p className="text-xs">Fechamento</p>
+            <p className="text-sm font-semibold">
+              {calculateDate(creditCard.closingDate)}
+            </p>
+          </div>
+
+          <div className="flex flex-col items-center justify-center">
+            <p className="text-xs">Vencimento</p>
+            <p className="text-sm font-semibold">
+              {calculateDate(creditCard.dueDate)}
+            </p>
+          </div>
+        </div>
+      </div>
     </Card>
   );
 };
