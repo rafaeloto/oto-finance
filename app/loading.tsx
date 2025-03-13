b4e1f32cf1509b3e1347dac2458d3e57a5a7f1ab
Rafael Oto <rafaelotoso@gmail.com>
2025-03-13 18:18:49 -0300

    feat: add background-paths component to loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,5 @@
-import { Loader2Icon } from "lucide-react";
+import { BackgroundPaths } from "./_components/ui/background-paths";
 
 export default function Loading() {
-  return (
-    <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-gray-900 bg-opacity-50">
-      <Loader2Icon className="animate-spin" size={50} />
-      <div className="ml-2">Carregando</div>
-    </div>
-  );
+  return <BackgroundPaths />;
 }
---
+++ app/test-background/page.tsx
@@ -0,0 +1,7 @@
+import { BackgroundPaths } from "../_components/ui/background-paths";
+
+const TestBackground = () => {
+  return <BackgroundPaths title="Carregando" />;
+};
+
+export default TestBackground;
