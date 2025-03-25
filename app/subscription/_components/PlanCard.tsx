0d678f551d00bb141ca9d63723bce4bbf1706e47
Rafael Oto <rafaelotoso@gmail.com>
2025-03-25 00:37:22 -0300

    feat: create PlanCard component

---
+++ app/subscription/_components/PlanCard.tsx
@@ -0,0 +1,64 @@
+import { Badge } from "@/app/_components/ui/badge";
+import { Card, CardContent, CardHeader } from "@/app/_components/ui/card";
+import { CheckIcon, XIcon } from "lucide-react";
+import AquirePlanButton from "./aquire-plan-button";
+import ShouldRender from "@/app/_components/_atoms/should-render";
+
+type PlanCardProps = {
+  isActive: boolean;
+  title: string;
+  value: number;
+  includedFeatures?: string[];
+  notIncludedFeatures?: string[];
+  showAquirePlanButton?: boolean;
+};
+
+const PlanCard = (props: PlanCardProps) => {
+  const {
+    isActive,
+    title,
+    value,
+    includedFeatures,
+    notIncludedFeatures,
+    showAquirePlanButton = false,
+  } = props;
+  return (
+    <Card className="w-[450px]">
+      <CardHeader className="relative border-b border-solid py-8">
+        {isActive && (
+          <Badge className="absolute left-4 top-8 bg-primary/10 text-base text-primary">
+            Ativo
+          </Badge>
+        )}
+        <h2 className="text-center text-2xl font-semibold">{title}</h2>
+        <div className="flex items-center justify-center gap-3">
+          <span className="text-4xl">R$</span>
+          <span className="text-6xl font-semibold">{value}</span>
+          <span className="text-2xl text-muted-foreground">/mês</span>
+        </div>
+      </CardHeader>
+
+      <CardContent className="space-y-6 py-8">
+        {includedFeatures?.map((feature) => (
+          <div className="flex items-center gap-2" key={feature}>
+            <CheckIcon className="text-primary" />
+            <p>{feature}</p>
+          </div>
+        ))}
+
+        {notIncludedFeatures?.map((feature) => (
+          <div className="flex items-center gap-2" key={feature}>
+            <XIcon />
+            <p>{feature}</p>
+          </div>
+        ))}
+
+        <ShouldRender if={showAquirePlanButton}>
+          <AquirePlanButton />
+        </ShouldRender>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default PlanCard;
--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,10 +1,7 @@
 import { auth, clerkClient } from "@clerk/nextjs/server";
 import Navbar from "../_components/_molecules/navbar";
 import { redirect } from "next/navigation";
-import { Card, CardContent, CardHeader } from "../_components/ui/card";
-import { CheckIcon, XIcon } from "lucide-react";
-import AquirePlanButton from "./_components/aquire-plan-button";
-import { Badge } from "../_components/ui/badge";
+import PlanCard from "./_components/PlanCard";
 import { getCurrentMonthTransactions } from "../_data/get-current-month-transactions";
 
 const SubscriptionPage = async () => {
@@ -25,61 +22,23 @@ const SubscriptionPage = async () => {
         <h1 className="text-2xl font-bold">Assinatura</h1>
 
         <div className="flex gap-10">
-          <Card className="w-[450px]">
-            <CardHeader className="border-b border-solid py-8">
-              <h2 className="text-center text-2xl font-semibold">
-                Plano Básico
-              </h2>
-              <div className="flex items-center justify-center gap-3">
-                <span className="text-4xl">R$</span>
-                <span className="text-6xl font-semibold">0</span>
-                <span className="text-2xl text-muted-foreground">/mês</span>
-              </div>
-            </CardHeader>
+          <PlanCard
+            isActive={!hasPremiumPlan}
+            title="Plano Básico"
+            value={0}
+            includedFeatures={[
+              `Apenas 10 transações por mês (${currentMonthTransactions}/10)`,
+            ]}
+            notIncludedFeatures={["Relatórios de IA"]}
+          />
 
-            <CardContent className="space-y-6 py-8">
-              <div className="flex items-center gap-2">
-                <CheckIcon className="text-primary" />
-                <p>
-                  Apenas 10 transações por mês ({currentMonthTransactions}/10)
-                </p>
-              </div>
-              <div className="flex items-center gap-2">
-                <XIcon />
-                <p>Relatórios de IA</p>
-              </div>
-            </CardContent>
-          </Card>
-
-          <Card className="w-[450px]">
-            <CardHeader className="relative border-b border-solid py-8">
-              {hasPremiumPlan && (
-                <Badge className="absolute left-4 top-8 bg-primary/10 text-base text-primary">
-                  Ativo
-                </Badge>
-              )}
-              <h2 className="text-center text-2xl font-semibold">
-                Plano Premium
-              </h2>
-              <div className="flex items-center justify-center gap-3">
-                <span className="text-4xl">R$</span>
-                <span className="text-6xl font-semibold">19</span>
-                <div className="text-2xl text-muted-foreground">/mês</div>
-              </div>
-            </CardHeader>
-
-            <CardContent className="space-y-6 py-8">
-              <div className="flex items-center gap-2">
-                <CheckIcon className="text-primary" />
-                <p>Transações ilimitadas</p>
-              </div>
-              <div className="flex items-center gap-2">
-                <CheckIcon className="text-primary" />
-                <p>Relatórios de IA</p>
-              </div>
-              <AquirePlanButton />
-            </CardContent>
-          </Card>
+          <PlanCard
+            isActive={hasPremiumPlan}
+            title="Plano Premium"
+            value={19}
+            includedFeatures={["Transações ilimitadas", "Relatórios de IA"]}
+            showAquirePlanButton
+          />
         </div>
       </div>
     </>
