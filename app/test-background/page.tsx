930cc2ab241c60d1672e34ef8e43837b3ab832af
Rafael Oto <rafaelotoso@gmail.com>
2025-03-25 01:42:31 -0300

    chore: remove test background page

--- app/test-background/page.tsx
+++
@@ -1,7 +0,0 @@
-import { BackgroundPaths } from "../_components/ui/background-paths";
-
-const TestBackground = () => {
-  return <BackgroundPaths title="Carregando" />;
-};
-
-export default TestBackground;
