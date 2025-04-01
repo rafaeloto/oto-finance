0c0f0ebe336fe2755830a26612338f95b5c0c997
Rafael Oto <rafaelotoso@gmail.com>
2025-04-01 19:18:46 -0300

    fix: useIsDesktop error

--- app/_utils/useIsDesktop.ts
+++ app/_utils/useIsDesktop.ts
@@ -1,5 +1,21 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 const useIsDesktop = () => {
-  const isDesktop = window.innerWidth > 768;
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsDesktop(window.innerWidth > 768);
+    };
+
+    checkScreenSize(); // Chama na montagem para definir o valor inicial
+
+    window.addEventListener("resize", checkScreenSize);
+
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
 
   return isDesktop;
 };
